98647a26b50ccc13158501416cfeb8f019647684
Si Lam <silam@hotmail.com>
2022-07-26 06:42:50 -0500

    Initial commit

---
+++ MappingObjectApp/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AutoMapper;
+using MappingObjectApp.Models;
+
+Console.WriteLine("Hello, World!");
+
+//Initialize the mapper
+var config = new MapperConfiguration(cfg =>
+        cfg.CreateMap<Employee, EmployeeDTO>()
+    );
+
+
+Employee emp = new Employee();
+emp.Name = "James";
+emp.Salary = 20000;
+emp.Address = "London";
+emp.Department = "IT";
+
+
+//Using automapper
+var mapper = new Mapper(config);
+var empDTO = mapper.Map<EmployeeDTO>(emp);
+//OR
+//var empDTO2 = mapper.Map<Employee, EmployeeDTO>(emp);
+
+
+//EmployeeDTO empDTO = new EmployeeDTO();
+//empDTO.Name = emp.Name;
+//empDTO.Salary = emp.Salary;
+//empDTO.Address = emp.Address;
+//empDTO.Department = emp.Department;
+
+
+Console.WriteLine("Name:" + empDTO.Name + ", Salary:" + empDTO.Salary + ", Address:" + empDTO.Address + ", Department:" + empDTO.Department);
+Console.ReadLine();
